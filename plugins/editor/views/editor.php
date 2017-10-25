b5dd80d1d45177fe5230512e3aa528ccaec38fbb
DaazKu <daazku@gmail.com>
2017-10-25 16:23:58 -0400

    Fix editor warning about "undefined index title"

--- plugins/editor/views/editor.php
+++ plugins/editor/views/editor.php
@@ -12,7 +12,7 @@ $html_arrow_down = '<span class="icon icon-caret-down" aria-hidden="true"></span
 $editor_file_input_name = $this->data('_editorFileInputName');
 
 foreach ($this->data('_EditorToolbar') as $button) {
-   $screenReaderMarkup = '<span class="sr-only">' . $button['attr']['title'] . '</span>';
+   $screenReaderMarkup = '<span class="sr-only">' . valr('attr.title', $button) . '</span>';
 
    // If the type is not an array, it's a regular button (type==button)
    if (!is_array($button['type'])) {
